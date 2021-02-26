9f1281fc1e061d6e5f52c4d260b796f53e2e8341
GKrisztian <g.krisztian@outlook.com>
2021-02-26 18:05:27 +0100

    new py/C implementation added

--- PlainC/main.c
+++ PlainC/main.c
@@ -2,23 +2,15 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
-#include <assert.h>
 
-#define PSEC 1.0e12
-#define NSEC 1.0e9
-#define USEC 1.0e6
-#define MSEC 1.0e3
-#define  SEC 1.0
-#define SC USEC
 
 double randbetween(double lower, double upper) {
     double sc = rand() / (double) RAND_MAX;
     return lower + sc * (upper - lower);
 }
 
-double ftri(double x, double p, double a)
-{
-    return 4 * a / p * fabs(fmod(x -p / 4, p)  - p/ 2) -a;
+double ftri(double x, double p, double a) {
+    return 4 * a / p * fabs(fmod(x - p / 4, p) - p / 2) - a;
 }
 
 int main() {
@@ -26,147 +18,164 @@ int main() {
     srand(time(NULL));
 
     double Vdc = 50.0;
-    double Vm = 20.0;
+    double Vm = 28.0;
+    double dtheta = 0.001;
+    double theta = 0.0;
 
-    double f_pwm = 10.0e3;
-    double f_out = 50.0;
-    double f_update = 1000;
-    double f_simulation = 500e3;
-    assert(f_simulation >= 2.0 * f_pwm);
+    double T_update = 1.0 / 500.0;
 
+    double Vi[8][2] = {
+            {0.0,              0.0},
+            {-1.0 / 3.0 * Vdc, -M_SQRT3 / 3.0 * Vdc},
+            {-1.0 / 3.0 * Vdc, M_SQRT3 / 3.0 * Vdc},
+            {-2.0 / 3.0 * Vdc, 0.0},
+            {2.0 / 3.0 * Vdc,  0.0},
+            {1.0 / 3.0 * Vdc,  -M_SQRT3 / 3.0 * Vdc},
+            {1.0 / 3.0 * Vdc,  M_SQRT3 / 3.0 * Vdc},
+            {0.0,              0.0},
+    };
 
-    double T_pwm = 1.0 / f_pwm;
-    double T_out = 1.0 / f_out;
-    double T_update = 1.0 / f_update;
-    double T_simulation = 1.0 / f_simulation;
-    double dt = T_simulation;
-
-    printf("T_pwm: %f s\n", T_pwm);
-    printf("T_out: %f s\n", T_out);
-    printf("T_update: %f s\n", T_update);
-    printf("T_simulation: %f s\n", T_simulation);
-    printf("dt: %f s\n", dt);
-
-    double t0 = 0.0, t1 = 0.0;
-    double dc[3] = {0.0};
+    unsigned int swt[8][3] = {
+            {0, 0, 0},
+            {0, 0, 1},
+            {0, 1, 0},
+            {0, 1, 1},
+            {1, 0, 0},
+            {1, 0, 1},
+            {1, 1, 0},
+            {1, 1, 1}
+    };
 
     do {
-        //t1 += randbetween(T_simulation / 2.0, T_simulation);
-        t1 += dt;
-        double elapsed = t1 - t0;
+        printf("%f\n", theta);
 
-        if (elapsed >= T_pwm || t0< 1.0e-10) {
-            t0 = t1;
+        double Vref[2] = {
+                Vm * cos(theta),
+                Vm * sin(theta)
+        };
+        double Vl[2] = {0.0};
+        double Vr[2] = {0.0};
+        unsigned int swl[3] = {0};
+        unsigned int swr[3] = {0};
 
-            double V[3] = {
-                    Vm * sin(2 * M_PI * f_out * t1 + 0 * M_PI * 2.0 / 3.0),
-                    Vm * sin(2 * M_PI * f_out * t1 + 1 * M_PI * 2.0 / 3.0),
-                    Vm * sin(2 * M_PI * f_out * t1 + 2 * M_PI * 2.0 / 3.0)
-            };
+        if (theta >= 0.0 && theta <= M_PI / 3.0) {
+            Vl[0] = Vi[6][0];
+            Vl[1] = Vi[6][1];
 
-            double Vref[2] = {
-                    1.0 / 3.0 * (2.0 * V[0] - V[1] - V[2]),
-                    M_SQRT3 / 3.0 * (V[1] - V[0])
-            };
+            Vr[0] = Vi[4][0];
+            Vr[1] = Vi[4][1];
 
-            double theta = atan2(Vref[1], Vref[0]) * 180.0 / M_PI;
+            swl[0] = swt[6][0];
+            swl[1] = swt[6][1];
+            swl[2] = swt[6][2];
 
-            unsigned char swl[3];
-            unsigned char swr[3];
+            swr[0] = swt[4][0];
+            swr[1] = swt[4][1];
+            swr[2] = swt[4][2];
+        } else if (theta >= M_PI / 3 && theta <= 2.0 / 3.0 * M_PI) {
+            Vl[0] = Vi[2][0];
+            Vl[1] = Vi[2][1];
 
-            if (theta >= 0.0 && theta <= 60.0) {
-                swl[0] = 1;
-                swl[1] = 1;
-                swl[2] = 0;
-                swr[0] = 1;
-                swr[1] = 0;
-                swr[2] = 0;
-            } else if (theta >= 60.0 && theta <= 120.0) {
-                swl[0] = 0;
-                swl[1] = 1;
-                swl[2] = 0;
-                swr[0] = 1;
-                swr[1] = 1;
-                swr[2] = 0;
-            } else if (theta >= 120.0 && theta <= 180.0) {
-                swl[0] = 0;
-                swl[1] = 1;
-                swl[2] = 1;
-                swr[0] = 0;
-                swr[1] = 1;
-                swr[2] = 0;
-            } else if (theta <= -120.0 && theta >= -180.0) {
-                swl[0] = 0;
-                swl[1] = 0;
-                swl[2] = 1;
-                swr[0] = 0;
-                swr[1] = 1;
-                swr[2] = 1;
-            } else if (theta <= -60.0 && theta >= -120.0) {
-                swl[0] = 1;
-                swl[1] = 0;
-                swl[2] = 1;
-                swr[0] = 0;
-                swr[1] = 0;
-                swr[2] = 1;
-            } else if (theta <= 0.0 && theta >= -60.0) {
-                swl[0] = 1;
-                swl[1] = 0;
-                swl[2] = 0;
-                swr[0] = 1;
-                swr[1] = 0;
-                swr[2] = 1;
-            }
-            double Vl[2] = {
-                    Vdc / 3.0 * (2 * swl[0] - swl[1] - swl[2]),
-                    Vdc * M_SQRT3 / 3.0 * (swl[1] - swl[2])
-            };
-            double Vr[2] = {
-                    Vdc / 3.0 * (2 * swr[0] - swr[1] - swr[2]),
-                    Vdc * M_SQRT3 / 3.0 * (swr[1] - swr[2])
-            };
+            Vr[0] = Vi[6][0];
+            Vr[1] = Vi[6][1];
 
-            double A[2][2] = {
-                    {Vl[0], Vr[0]},
-                    {Vl[1], Vr[1]}
-            };
-            double detA = A[0][0] * A[1][1] - A[0][1] * A[1][0];
+            swl[0] = swt[2][0];
+            swl[1] = swt[2][1];
+            swl[2] = swt[2][2];
 
-            double invA[2][2] = {
-                    {A[1][1]/detA, -A[0][1]/detA},
-                    {-A[1][0]/detA, A[0][0]/detA}
-            };
+            swr[0] = swt[6][0];
+            swr[1] = swt[6][1];
+            swr[2] = swt[6][2];
+        } else if (theta >= 2.0 / 3.0 * M_PI && theta <= M_PI) {
+            Vl[0] = Vi[3][0];
+            Vl[1] = Vi[3][1];
 
-            double b[2] = {
-                    T_pwm * Vref[0],
-                    T_pwm * Vref[1]
-            };
+            Vr[0] = Vi[2][0];
+            Vr[1] = Vi[2][1];
 
-            double T[2] = {
-                invA[0][0] * b[0] + invA[0][1] * b[1],
-                invA[1][0] * b[0] + invA[1][1] * b[1]
-            };
+            swl[0] = swt[3][0];
+            swl[1] = swt[3][1];
+            swl[2] = swt[3][2];
 
-            double Toff = T_pwm - T[0] - T[1];
+            swr[0] = swt[2][0];
+            swr[1] = swt[2][1];
+            swr[2] = swt[2][2];
+        } else if (theta >= M_PI && theta <= 4.0 / 3.0 * M_PI) {
+            Vl[0] = Vi[1][0];
+            Vl[1] = Vi[1][1];
 
-            dc[0] = (swl[0]*T[0] + swr[0]*T[1] + Toff / 2) / T_pwm;
-            dc[1] = (swl[1]*T[0] + swr[1]*T[1] + Toff / 2) / T_pwm;
-            dc[2] = (swl[2]*T[0] + swr[2]*T[1] + Toff / 2) / T_pwm;
+            Vr[0] = Vi[3][0];
+            Vr[1] = Vi[3][1];
+
+            swl[0] = swt[1][0];
+            swl[1] = swt[1][1];
+            swl[2] = swt[1][2];
+
+            swr[0] = swt[3][0];
+            swr[1] = swt[3][1];
+            swr[2] = swt[3][2];
+        } else if (theta >= 4.0 / 3.0 * M_PI && theta <= 5.0 / 3.0 * M_PI) {
+            Vl[0] = Vi[5][0];
+            Vl[1] = Vi[5][1];
+
+            Vr[0] = Vi[1][0];
+            Vr[1] = Vi[1][1];
+
+            swl[0] = swt[5][0];
+            swl[1] = swt[5][1];
+            swl[2] = swt[5][2];
+
+            swr[0] = swt[1][0];
+            swr[1] = swt[1][1];
+            swr[2] = swt[1][2];
+        } else if (theta >= 5.0 / 3.0 * M_PI && theta <= 2.0 * M_PI) {
+            Vl[0] = Vi[4][0];
+            Vl[1] = Vi[4][1];
+
+            Vr[0] = Vi[5][0];
+            Vr[1] = Vi[5][1];
+
+            swl[0] = swt[4][0];
+            swl[1] = swt[4][1];
+            swl[2] = swt[4][2];
+
+            swr[0] = swt[5][0];
+            swr[1] = swt[5][1];
+            swr[2] = swt[5][2];
         }
 
-        double vtr = (0.5 + 0.5 * ftri(t1+T_pwm / 4, T_pwm, 1.0));
-        double pwm[3] = {0.0};
-        if (dc[0] >= vtr) { pwm[0] = 1.0;}
-        if (dc[1] >= vtr) { pwm[1] = 1.0;}
-        if (dc[2] >= vtr) { pwm[2] = 1.0;}
+        double A[2][2] = {
+                {Vl[0], Vr[0]},
+                {Vl[1], Vr[1]}
+        };
 
+        double detA = A[0][0] * A[1][1] - A[0][1] * A[1][0];
+        double invA[2][2] = {
+                {A[1][1] / detA,  -A[0][1] / detA},
+                {-A[1][0] / detA, A[0][0] / detA}
+        };
+        double b[2] = {
+                T_update * Vref[0],
+                T_update * Vref[1],
+        };
 
+        double T[2] = {
+                invA[0][0] * b[0] + invA[0][1] * b[1],
+                invA[1][0] * b[0] + invA[1][1] * b[1],
+        };
 
-        //printf("t1: %.15f\n", t1);
-        fprintf(fp, "%.15f, %f,%f,%f,", t1, dc[0], dc[1], dc[2]);
-        fprintf(fp, "%f,%f,%f, %f\n", pwm[0], pwm[1], pwm[2], vtr);
+        double Toff = T_update - T[0] - T[1];
+        double dc[3] = {
+                1.0 / T_update * (swl[0] * T[0] + swr[0] * T[1] + Toff / 2.0),
+                1.0 / T_update * (swl[1] * T[0] + swr[1] * T[1] + Toff / 2.0),
+                1.0 / T_update * (swl[2] * T[0] + swr[2] * T[1] + Toff / 2.0),
+        };
 
-    } while (t1 <= 2 * T_out);
+        fprintf(fp, "%f,%f,%f,%f\n", theta, dc[0], dc[1], dc[2]);
+
+        theta += dtheta;
+    } while (theta <= 2 * M_PI);
+
 
     fclose(fp);
 
